522c6af82f7265f3e2b52b0ad779b1e9628e2d96
ArisAg <aris.18.08.ap@gmail.com>
2018-01-22 16:13:42 -0600

    Fixed of lift drop and corrections

--- robot.py
+++ robot.py
@@ -14,22 +14,13 @@ class MyRobot(wpilib.IterativeRobot):
 		"""
 		This function is called upon program startup and
 		should be used for any initialization code.
-		"""
-		self.frontLeft = wpilib.Jaguar(1)
-		self.rearLeft = wpilib.Jaguar(2)
-		self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.rearLeft)
+        """
 
-		self.frontRight = wpilib.Jaguar(3)
-		self.rearRight = wpilib.Jaguar(4)
-		self.right = wpilib.SpeedControllerGroup(self.frontRight, self.rearRight)
-
-		self.drive = wpilib.drive.DifferentialDrive(self.left, self.right)
-
-		self.stick = wpilib.Joystick(1)
+		self.stick = wpilib.Joystick(0)
 		self.timer = wpilib.Timer()
-		self.lift1 = wpilib.Jaguar(5)
-		self.lift2 = wpilib.Jaguar(6)
-		self.drop = wpilib.Jaguar(7)
+		self.lift1 = wpilib.Jaguar(4)
+		self.lift2 = wpilib.Jaguar(5)
+		self.drop = wpilib.Jaguar(6)
 
 		self.button_RB = JoystickButton(self.stick, 1)
 		self.button_LB = JoystickButton(self.stick, 2)
@@ -39,20 +30,11 @@ class MyRobot(wpilib.IterativeRobot):
 		"""This function is run once each time the robot enters autonomous mode."""
 		self.timer.reset()
 		self.timer.start()
-
-	def autonomousPeriodic(self):
-		"""This function is called periodically during autonomous."""
-
-		# Drive for two seconds
-		if self.timer.get() < 2.0:
-			self.drive.arcadeDrive(-0.5, 0)  # Drive forwards at half speed
-		else:
-			self.drive.arcadeDrive(0, 0)  # Stop robot
+	
 
 	def teleopPeriodic(self):
 		"""This function is called periodically during operator control."""
 
-		self.drive.arcadeDrive(self.stick.getY(), self.stick.getX())
 		
 		if self.button_RB.get():
 			self.lift1.set(1)
