a4e79f216c2bec1dff1e6a2b6a01c150dde4a7ed
ArisAg <aris.18.08.ap@gmail.com>
2018-01-22 15:54:21 -0600

    fixed turbo code & chasis orientation

--- robot.py
+++ robot.py
@@ -15,16 +15,16 @@ class MyRobot(wpilib.IterativeRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
-        self.frontLeft = wpilib.Jaguar(1)
-        self.rearLeft = wpilib.Jaguar(2)
+        self.frontLeft = wpilib.Jaguar(0)
+        self.rearLeft = wpilib.Jaguar(1)
         self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.rearLeft)
 
-        self.frontRight = wpilib.Jaguar(3)
-        self.rearRight = wpilib.Jaguar(4)
+        self.frontRight = wpilib.Jaguar(2)
+        self.rearRight = wpilib.Jaguar(3)
         self.right = wpilib.SpeedControllerGroup(self.frontRight, self.rearRight)
 
         self.drive = wpilib.drive.DifferentialDrive(self.left, self.right)
-        self.stick = wpilib.Joystick(1)
+        self.stick = wpilib.Joystick(0)
         self.timer = wpilib.Timer()
         self.button_a = JoystickButton(self.stick, 1)
 
@@ -44,20 +44,22 @@ class MyRobot(wpilib.IterativeRobot):
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        if self.stick.getY() < 0.1 and self.stick.getX() < 0.1 and self.stick.getY() > -0.1 and self.stick.getX() > -0.1:
+        if self.stick.getX() < 0.1 and self.stick.getY() < 0.1 and self.stick.getX() > -0.1 and self.stick.getY() > -0.1:
             self.drive.arcadeDrive(0,0)
         else: 
-            powerX = self.stick.getX()
             powerY = self.stick.getY()
+            powerX = self.stick.getX()
 
             if self.button_a.get():
-                powerX *= 1
                 powerY *= 1
+
+ 
             else:
-                powerX *= 0.7
                 powerY *= 0.7
 
-            self.drive.arcadeDrive(powerX, powerY)
+
+
+            self.drive.arcadeDrive(powerY, powerX)
 
 
 
