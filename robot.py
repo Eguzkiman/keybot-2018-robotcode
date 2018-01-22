6931be50e3758b82ae66f3cec12a2b6b557f7ce0
Lau Rivero <laurivero@MacBook-Pro-de-Lau.local>
2018-01-22 17:19:44 -0600

    auto

--- robot.py
+++ robot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-    This is a good foundation to build your robot code on
+	This is a good foundation to build your robot code on
 """
 
 import wpilib
@@ -9,70 +9,86 @@ import wpilib.drive
 
 class MyRobot(wpilib.IterativeRobot):
 
-    def robotInit(self):
-        """
-        This function is called upon program startup and
-        should be used for any initialization code.
-        """
+	def robotInit(self):
+		"""
+		This function is called upon program startup and
+		should be used for any initialization code.
+		"""
 
-        self.frontLeft = wpilib.Jaguar(0)
-        self.rearLeft = wpilib.Jaguar(1)
-        self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.rearLeft)
+		self.frontLeft = wpilib.Jaguar(0)
+		self.rearLeft = wpilib.Jaguar(1)
+		self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.rearLeft)
 
-        self.frontRight = wpilib.Jaguar(2)
-        self.rearRight = wpilib.Jaguar(3)
-        self.right = wpilib.SpeedControllerGroup(self.frontRight, self.rearRight)
+		self.frontRight = wpilib.Jaguar(2)
+		self.rearRight = wpilib.Jaguar(3)
+		self.right = wpilib.SpeedControllerGroup(self.frontRight, self.rearRight)
 
-        self.drive = wpilib.drive.DifferentialDrive(self.left, self.right)
-        self.stick = wpilib.Joystick(0)
-        self.timer = wpilib.Timer()
-        self.lift1 = wpilib.Jaguar(4)
-        self.lift2 = wpilib.Jaguar(5)
-        self.drop = wpilib.Jaguar(6)
+		self.drive = wpilib.drive.DifferentialDrive(self.left, self.right)
+		self.stick = wpilib.Joystick(0)
+		self.timer = wpilib.Timer()
+		self.lift1 = wpilib.Jaguar(4)
+		self.lift2 = wpilib.Jaguar(5)
+		self.drop = wpilib.Jaguar(6)
 
-        self.button_RB = JoystickButton(self.stick, 1)
+		self.button_RB = JoystickButton(self.stick, 1)
 		self.button_LB = JoystickButton(self.stick, 2)
 		self.button_B = JoystickButton(self.stick, 3)
 
 
-    def autonomousInit(self):
-        """This function is run once each time the robot enters autonomous mode."""
-        self.timer.reset()
-        self.timer.start()
+	def autonomousInit(self):
+		"""This function is run once each time the robot enters autonomous mode."""
+		self.timer.reset()
+		self.timer.start()
 
-    def autonomousPeriodic(self):
-        """This function is called periodically during autonomous."""
+	def autonomousPeriodic(self):
+		"""This function is called periodically during autonomous."""
 
-        # Drive for two seconds
-        if self.timer.get() < 2.0:
-            self.drive.arcadeDrive(1, 0)  
-        elif self.timer.get() < 4.0:
-            self.lift1.set(1)
-            self.lift2.set(1)
-        elif self.timer.get() < 5.5:
-            self.drop.set(1)
-            self.lift1.set(0)
-            self.lift2.set(0)
-        elif self.timer.get() < 7.5:
-            self.lift1.set(-1)
-            self.lift2.set(-1)
-            self.drop.set(0)
-        elif self.timer.get() < 9.5:
-            self.drive.arcadeDrive(-1, 0)  
-            self.lift1.set(0)
-            self.lift2.set(0)
+		# Drive for two seconds
+		if self.timer.get() < 2.0:
+			self.drive.arcadeDrive(1, 0)  
+		elif self.timer.get() < 4.0:
+			self.lift1.set(1)
+			self.lift2.set(1)
+		elif self.timer.get() < 5.5:
+			self.drop.set(1)
+			self.lift1.set(0)
+			self.lift2.set(0)
+		elif self.timer.get() < 7.5:
+			self.lift1.set(-1)
+			self.lift2.set(-1)
+			self.drop.set(0)
+		elif self.timer.get() < 9.5:
+			self.drive.arcadeDrive(-1, 0)  
+			self.lift1.set(0)
+			self.lift2.set(0)
 
-        else:
-            self.drive.arcadeDrive(0, 0)
-            self.lift1.set(0)
-            self.lift2.set(0)
-            self.drop.set(0)
+		else:
+			self.drive.arcadeDrive(0, 0)
+			self.lift1.set(0)
+			self.lift2.set(0)
+			self.drop.set(0)
 
-              # Stop robot
+			  # Stop robot
 
    def teleopPeriodic(self):
-		"""This function is called periodically during operator control."""
 
+		if self.stick.getX() < 0.1 and self.stick.getY() < 0.1 and self.stick.getX() > -0.1 and self.stick.getY() > -0.1:
+			self.drive.arcadeDrive(0,0)
+		else: 
+			powerY = self.stick.getY()
+			powerX = self.stick.getX()
+
+			if self.button_a.get():
+				powerY *= 1
+
+ 
+			else:
+				powerY *= 0.7
+
+
+
+			self.drive.arcadeDrive(powerY, powerX)
+		
 		
 		if self.button_RB.get():
 			self.lift1.set(1)
@@ -98,4 +114,4 @@ class MyRobot(wpilib.IterativeRobot):
 
 
 if __name__ == "__main__":
-    wpilib.run(MyRobot)
+	wpilib.run(MyRobot)
